f4e61633ef14c591a6bd061155a8024ad4de8a06
Rahul2302001 <rahul2001official@gmail.com>
2023-04-02 00:46:33 +0530

    main

--- js/styleswitcher.js
+++ js/styleswitcher.js
@@ -16,10 +16,15 @@ window.addEventListener("scroll", () => {
 const alternateStyles = document.querySelectorAll(".alternate-style");
 function setActiveStyle(color)
 {
+
     alternateStyles.forEach((style) => {
         if(color === style.getAttribute("title"))
         {
             style.removeAttribute("disabled");
+            if(document.querySelector(".style-switcher").classList.contains("open"))
+        {
+        document.querySelector(".style-switcher").classList.remove("open");
+        }
         }
         else
         {
@@ -37,11 +42,11 @@ dayNight.addEventListener("click", () => {
 window.addEventListener("load", () => {
     if(document.body.classList.contains("dark"))
     {
-        dayNight.querySelector("i").classList.add("fa-sun");
+        dayNight.querySelector("i").classList.add("fa-moon");
     }
     else
     {
-        dayNight.querySelector("i").classList.add("fa-moon");
+        dayNight.querySelector("i").classList.add("fa-sun");
     }
     
 })
